679cf79c59f4bbd63050d91ae7fa172c7e97b7a9
Ewan Harris <ewanharris93@gmail.com>
2019-04-25 11:55:22 +0100

    add json flag

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const { run } = require('appcd-subprocess');
 
-run('npm', [ 'install', '-g', `appcelerator@4.2.13`], { shell: true })
+run('npm', [ 'install', '-g', `appcelerator@4.2.13`, '--json'], { shell: true })
 	.then(out => {
 		console.log(out);
 	})
